22d6083efb8b3557ced0fd39e0f776406cd6df1f
marine <qazsw153@163.com>
2023-09-06 20:46:54 +0800

    debug:浅拷贝导致map冲突

--- api_router/router/middleware/serve.go
+++ api_router/router/middleware/serve.go
@@ -8,6 +8,7 @@ import (
 
 func ServeMiddleware(serveInstance map[string]interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 如果直接复制，浅拷贝会导致map冲突
 		c.Keys = make(map[string]interface{})
 		for key, value := range serveInstance {
 			c.Keys[key] = value
